3807dc73f70b29181e41cd870a178e4061f648c7
he110world <he110world@126.com>
2012-02-17 23:51:42 +0800

    upload files

---
+++ helloworld.js
@@ -0,0 +1,8 @@
+function fuckYouAll()
+{
+	var canvas = document.getElementById("fuckCanvas");			
+	var fuck = canvas.getContext("2d");
+	
+	fuck.fillStyle="#00FF00"
+	fuck.fillRect(0,0,200,100);	
+}
